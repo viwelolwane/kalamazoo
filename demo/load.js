4236e3419185f008dacb0ae6841036a1d3fed179
viwe <vlolwane@g.havard.edu>
2015-04-07 00:52:47 -0700

    first commit

---
+++ demo/load.js
@@ -0,0 +1,91 @@
+var widgets = [ 'map','bar', 'line' ];
+
+var DemoGrid = {
+  currentSize: 3,
+  buildElements: function($gridContainer, items) {
+    var item, i, temp;
+    for (i = 0; i < 7; i++) {
+      item = items[i];
+      if(widgets[i]=="bar"){
+        temp =  '<div><canvas id=' + widgets[i] + ' height="250">' + '</canvas></div>';
+      } else {
+        temp = '<div><div class=' + widgets[i] + '/>' + '</div>';
+      }
+      $item = $(
+        '<li>' +
+          '<div class="inner">' +
+            '<div class="controls">' +
+              '<a href="#zoom1" class="resize" data-size="1">1x</a>' +
+              '<a href="#zoom2" class="resize" data-size="2">2x</a>' +
+              '<a href="#zoom3" class="resize" data-size="3">3x</a>' +
+            '</div>' +
+            temp +
+          '</div>' +
+        '</li>'
+      );
+      $item.attr({
+        'data-w': 3,
+        'data-h': 2,
+        'data-x': item.x,
+        'data-y': item.y
+      });
+      $gridContainer.append($item);
+    }
+  },
+  resize: function(size) {
+    if (size) {
+      this.currentSize = size;
+    }
+    $('#grid').gridList('resize', this.currentSize);
+  },
+  flashItems: function(items) {
+    // Hack to flash changed items visually
+    for (var i = 0; i < items.length; i++) {
+      (function($element) {
+        $element.addClass('changed')
+        setTimeout(function() {
+          $element.removeClass('changed');
+        }, 0);
+      })(items[i].$element);
+    }
+  },
+  renderwidgets: function(){
+    $(document).ready(function(){
+      //mapwidget();
+      histogramwidget();
+      graphwidget();
+    });
+  } 
+};
+
+$(window).resize(function() {
+  DemoGrid.resize();
+});
+
+$(function() {
+  DemoGrid.buildElements($('#grid'), fixtures.DEMO);
+  DemoGrid.renderwidgets();
+
+  $('#grid').gridList({
+    rows: DemoGrid.currentSize,
+    widthHeightRatio: 264 / 294,
+    heightToFontSizeRatio: 0.25,
+    onChange: function(changedItems) {
+      DemoGrid.flashItems(changedItems);
+    }
+  });
+  $('#grid li .resize').click(function(e) {
+    e.preventDefault();
+    var itemElement = $(e.currentTarget).closest('li'),
+        itemSize = $(e.currentTarget).data('size');
+    $('#grid').gridList('resizeItem', itemElement, itemSize);   
+  });
+  $('.add-row').click(function(e) {
+    e.preventDefault();
+    DemoGrid.resize(DemoGrid.currentSize + 1);
+  });
+  $('.remove-row').click(function(e) {
+    e.preventDefault();
+    DemoGrid.resize(Math.max(1, DemoGrid.currentSize - 1));
+  });
+});
---
+++ widgets/widgets.js
@@ -0,0 +1,95 @@
+mapwidget = function(){
+    var map = new ol.Map({
+    target: $('#map')[0],
+    layers: [
+        new ol.layer.Tile({
+            source: new ol.source.OSM()
+        })
+    ],
+    view: new ol.View({
+        center: ol.proj.transform([28.2786, -25.7556], 'EPSG:4326', 'EPSG:3857'),
+        zoom: 18
+    })
+   });
+}
+
+
+histogramwidget = function(){
+    var randomScalingFactor = function(){ return Math.round(Math.random()*1400)};
+
+    var barChartData = {
+        labels : ["January","February","March","April","May","June","July"],
+        datasets : [
+            {
+                fillColor : "rgba(151,187,205,0.5)",
+                strokeColor : "rgba(151,187,205,0.8)",
+                highlightFill : "rgba(151,187,205,0.75)",
+                highlightStroke : "rgba(151,187,205,1)",
+                data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+            }
+        ]
+    }
+
+    var ctx = $('#bar')[0].getContext("2d");
+    myBar = new Chart(ctx).Bar(barChartData, {
+        responsive : false
+    });
+}
+
+
+graphwidget = function(){
+    var seriesData = [ [] ];
+    var random = new Rickshaw.Fixtures.RandomData(150);
+
+    for (var i = 0; i < 150; i++) {
+        random.addData(seriesData);
+    }
+
+    var palette = new Rickshaw.Color.Palette( { scheme: 'classic9' } );
+
+    // instantiate our graph!
+
+    alert($('#line').width());
+    var graph = new Rickshaw.Graph( {
+        element: $('.line')[0],
+        renderer: 'line',
+        height: $('#line').parent().attr("height"),
+        width: $('#line').parent().attr("width"),
+        stroke: true,
+        preserve: true,
+        series: [
+            {
+                color: '#6060c0',
+                data: seriesData[0],
+                name: 'Moscow'
+            }
+        ]
+    } );
+
+    graph.render();
+
+    var ticksTreatment = 'glow';
+
+    var xAxis = new Rickshaw.Graph.Axis.Time( {
+        graph: graph,
+        ticksTreatment: ticksTreatment,
+        timeFixture: new Rickshaw.Fixtures.Time.Local()
+    } );
+
+    xAxis.render();
+
+    var yAxis = new Rickshaw.Graph.Axis.Y( {
+        graph: graph,
+        tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
+        ticksTreatment: ticksTreatment
+    } );
+
+    yAxis.render();
+
+    setInterval( function() {
+        random.removeData(seriesData);
+        random.addData(seriesData);
+        graph.update();
+
+    }, 50 );
+}
